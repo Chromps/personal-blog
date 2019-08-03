1566178cea9ea03d54086f2f118c7c004a8603e2
Cody Swift <ccswift@me.com>
2019-08-02 22:17:18 -0400

    Initial commit, base repo with config updates

---
+++ README.md
@@ -0,0 +1,5 @@
+# A blog
+
+A simple blog I will be developing, using [next.js blog starter](https://github.com/zeit/next.js/tree/canary/examples/blog-starter).
+
+For more information about why I have chosen this as a starter, see this blog at https://www.codyswift.com
---
+++ blog.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    siteMeta: {
+        title: "Ramblings About Software and Life",
+        author: "Cody Swift",
+        image: "/static/site-feature.png",
+        description:
+            "A country boy explores the world of software engineering, and makes lots of typos along the way.",
+        siteUrl: "https://www.codyswift.com",
+        social: {
+            twitter: "chromps"
+        },
+        postsPerPage: 5
+    }
+}
