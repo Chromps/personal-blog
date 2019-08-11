56754bbb7530f0c01048d9a0a3b4cdf1461c1e0d
Cody Swift <ccswift@me.com>
2019-08-10 22:38:15 -0400

    Update width of page to 75 rem, auto format container.js
     Changes to be committed:
    	modified:   components/container.js

--- components/container.js
+++ components/container.js
@@ -1,12 +1,12 @@
 const Container = ({ children }) => (
-  <>
-    <div>{children}</div>
-    <style jsx>{`
-      max-width: 45rem;
-      margin: 0 auto;
-      padding: 0 1em;
-    `}</style>
-  </>
+    <>
+        <div>{children}</div>
+        <style jsx>{`
+            max-width: 75rem;
+            margin: 0 auto;
+            padding: 0 1em;
+        `}</style>
+    </>
 )
 
 export default Container
